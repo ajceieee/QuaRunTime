2558155ed42175c87d5b753791eb2f52023c46cd
tonalmathew <tonalmathew1999@gmail.com>
2021-05-31 20:56:46 +0530

    updated faq

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -6,12 +6,12 @@ for (let i = 0; i < toggles.length; i++) {
 	toggles[i].addEventListener("click", () => {
 		if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
 			contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
-			toggles[i].style.color = "#111130";
+			toggles[i].style.color = "#f59800";
 			icons[i].classList.remove("fa-plus");
 			icons[i].classList.add("fa-minus");
 		} else {
 			contentDiv[i].style.height = "0px";
-			toggles[i].style.color = "#111130";
+			toggles[i].style.color = "#f59800";
 			icons[i].classList.remove("fa-minus");
 			icons[i].classList.add("fa-plus");
 		}
@@ -19,7 +19,7 @@ for (let i = 0; i < toggles.length; i++) {
 		for (let j = 0; j < contentDiv.length; j++) {
 			if (j !== i) {
 				contentDiv[j].style.height = 0;
-				toggles[j].style.color = "#111130";
+				toggles[j].style.color = "#f59800";
 				icons[j].classList.remove("fa-minus");
 				icons[j].classList.add("fa-plus");
 			}
